cfcf0d86b60416312dbc630931eeac250aec6afb
FuadTalukder85 <webfuadtk@gmail.com>
2023-06-04 18:40:16 +0600

    solve error

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const verifyJWt = (req, res, next) => {
+const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization;
     if (!authorization) {
         return res.status(401).send({ error: true, message: 'unauthorized access' });
     }
     //bearer token
-    const token = authorization.split(' ');
+    const token = authorization.split(' ')[1];
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
@@ -51,13 +51,28 @@ async function run() {
 
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' })
 
             res.send({ token })
         })
 
+
+        //Warning: use verifyJWT before using verifyAdmin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' })
+            }
+            next();
+        }
+
+
+
+
         //users releted apis
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
         })
@@ -79,7 +94,7 @@ async function run() {
         // security layer: verifyJWT
         //email same
         // admin same
-        app.get('/users/admin/:email', verifyJWt, async (req, res) => {
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
 
             if (req.decoded.email !== email) {
@@ -87,7 +102,7 @@ async function run() {
             }
 
             const query = { email: email };
-            const user = await usersCollection.find(query);
+            const user = await usersCollection.findOne(query);
             const result = { admin: user?.role === 'admin' }
             res.send(result)
         })
@@ -121,7 +136,7 @@ async function run() {
         })
 
         // cart collection apis
-        app.get('/carts', verifyJWt, async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
             if (!email) {
                 res.send([]);
